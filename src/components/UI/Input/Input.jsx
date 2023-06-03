8ac0b793aa42527a30a55affa4419faa5a793ec5
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-03 20:26:57 +0100

    create Input component

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import FIELD_TYPES from '../../../enums/field-types';
+
+import classes from './Input.module.css';
+
+export default function Input({
+  name,
+  type = FIELD_TYPES.TEXT,
+  useLabel = true,
+  label,
+  placeholder,
+  isSingleLine = false,
+  hasError = false,
+  errorMessage,
+  onChange,
+  onBlur,
+  value,
+  className,
+}) {
+  const getContainerClasses = () => {
+    let containerClasses = classes.container;
+
+    if (isSingleLine) {
+      containerClasses += ` ${classes.singleline}`;
+    }
+
+    if (hasError) {
+      containerClasses += ` ${classes.invalid}`;
+    }
+
+    if (className) {
+      containerClasses += ` ${className}`;
+    }
+
+    return containerClasses;
+  };
+
+  const inputHandler = (type) => {
+    switch (type) {
+      case FIELD_TYPES.TEXTAREA:
+        return (
+          <textarea
+            name={name}
+            placeholder={placeholder}
+            rows={5}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        );
+      default:
+        return (
+          <input
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        );
+    }
+  };
+
+  return (
+    <div className={getContainerClasses()}>
+      {useLabel && <label>{(label ??= placeholder)}:</label>}
+      {inputHandler(type)}
+      {hasError && <p className={classes.error}>{errorMessage}</p>}
+    </div>
+  );
+}
---
+++ src/enums/field-types.js
@@ -0,0 +1,10 @@
+const FIELD_TYPES = {
+  TEXT: 'text',
+  TEXTAREA: 'textarea',
+  NUMBER: 'number',
+  DATE: 'date',
+  EMAIL: 'email',
+  PASSWORD: 'password',
+};
+
+export default FIELD_TYPES;
