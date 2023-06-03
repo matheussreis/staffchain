f0495d15870a3408bd437749746edab382fe5d8d
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-03 23:52:08 +0100

    create a custom hook for keeping track of the steps in a multi-step form

---
+++ src/hooks/use-multi-step-form.js
@@ -0,0 +1,46 @@
+import { useReducer } from 'react';
+
+const initialState = {
+  steps: [],
+  currentStep: 0,
+};
+
+const multiStepFormReducer = (state, action) => {
+  switch (action.type) {
+    case 'NEXT':
+      return { steps: state.steps, currentStep: state.currentStep + 1 };
+    case 'PREVIOUS':
+      return { steps: state.steps, currentStep: state.currentStep - 1 };
+    default:
+      return initialState;
+  }
+};
+
+const useMultiStepForm = (steps) => {
+  if (steps) {
+    initialState.steps = steps;
+  }
+
+  const [state, dispatch] = useReducer(multiStepFormReducer, initialState);
+
+  const goToNextStep = () => {
+    if (state.currentStep + 1 <= state.steps.length - 1) {
+      dispatch({ type: 'NEXT' });
+    }
+  };
+
+  const goToPreviousStep = () => {
+    if (state.currentStep - 1 >= 0) {
+      dispatch({ type: 'PREVIOUS' });
+    }
+  };
+
+  return {
+    steps: state.steps,
+    currentStep: state.currentStep,
+    goToPreviousStep,
+    goToNextStep,
+  };
+};
+
+export default useMultiStepForm;
