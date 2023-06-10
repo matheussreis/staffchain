4cf3af3d3ab20b227e6a913624f4d2a0c5d59ba5
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-11 00:47:42 +0100

    handle redirect state in the User component

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,15 +1,17 @@
 import EditUser from './EditUser';
-import { useParams } from 'react-router';
 import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router';
 import useEditPageCheck from '../../hooks/use-edit-page-check';
 import { UserFormContext } from '../../store/user-form-context';
 import UserFormView from '../../components/UserForm/UserFormView';
 
 export default function User() {
+  const location = useLocation();
   const { id: userId } = useParams();
   const isEdit = useEditPageCheck('user');
 
   const [fields, setFields] = useState({
+    id: { value: '' },
     firstName: { value: '' },
     lastName: { value: '' },
     email: { value: '' },
@@ -20,20 +22,49 @@ export default function User() {
     password: { value: '' },
   });
 
+  // Use this component as the parent of all user related views,
+  // in other words, the parent of the view, edit and list views.
+  // Then, add logic to fetch a list or a single user depending
+  // on the URL. Lastly, add logic to select the component that
+  // should be rendered. In case the URL is for list, then a list
+  // of users should be rendered, in case the URL is for view, a
+  // single user should be rendered.
+
   useEffect(() => {
-    // Call API using the user ID to fetch the user data.
-    console.log(`USER ID = ${userId} | FETCH USER DATA TO DISPLAY`);
-    setFields({
-      firstName: { value: 'John' },
-      lastName: { value: 'Doe' },
-      email: { value: 'johndoe@test.com' },
-      phone: { value: '123321123' },
-      birthdate: { value: '1997-01-01' },
-      isAdministrator: { value: { label: 'Yes', value: '1' } },
-      departmentName: { value: 'Marketing' },
-      password: { value: '1234567890' },
-    });
-  }, [userId]);
+    if (fields.id.value !== '') {
+      return;
+    }
+
+    if (location.state) {
+      // Don't call the API, and use the given state.
+      console.log(`USER ID = ${userId} | USE USER DATA FROM STATE`);
+      setFields({
+        id: { value: location.state.id },
+        firstName: { value: location.state.firstName },
+        lastName: { value: location.state.lastName },
+        email: { value: location.state.email },
+        phone: { value: location.state.phone },
+        birthdate: { value: location.state.birthdate },
+        isAdministrator: { value: location.state.isAdministrator },
+        departmentName: { value: location.state.departmentName },
+        password: { value: location.state.password },
+      });
+    } else {
+      // Call API using the user ID to fetch the user data.
+      console.log(`USER ID = ${userId} | USE USER DATA FROM API`);
+      setFields({
+        id: { value: '1000' },
+        firstName: { value: 'John' },
+        lastName: { value: 'Doe' },
+        email: { value: 'johndoe@test.com' },
+        phone: { value: '123321123' },
+        birthdate: { value: '1997-01-01' },
+        isAdministrator: { value: { label: 'Yes', value: '1' } },
+        departmentName: { value: 'Marketing' },
+        password: { value: '1234567890' },
+      });
+    }
+  }, [userId, location.state, fields.id.value]);
 
   const UserComponent = isEdit ? EditUser : UserFormView;
 
