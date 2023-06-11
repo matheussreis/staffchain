c7e756cb6ebe2b60f4c605a7cccbf41e4d038e71
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-11 11:52:20 +0100

    add state option to the Anchor component

--- src/components/UI/Anchor/Anchor.jsx
+++ src/components/UI/Anchor/Anchor.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 import classes from './Anchor.module.css';
 
-export default React.memo(function Anchor({ to, className, children }) {
+export default React.memo(function Anchor({ to, className, state, children }) {
   return (
-    <Link className={`${classes.anchor} ${className}`} to={to}>
+    <Link className={`${classes.anchor} ${className}`} to={to} state={state}>
       {children}
     </Link>
   );
