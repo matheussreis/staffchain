369d3eaf3c190a467d204f7cf326c2b07c758869
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-26 16:58:15 +0100

    add support for file fields in the FieldView component

--- src/components/UI/FieldView/FieldView.jsx
+++ src/components/UI/FieldView/FieldView.jsx
@@ -3,20 +3,28 @@ import FIELD_TYPES from '../../../enums/field-types';
 import classes from './FieldView.module.css';
 
 const getFormattedContent = (content, type) => {
-  // add validation for content as a date
-  if (type === FIELD_TYPES.DATE) {
-    const date = new Date(content);
-    return date.toLocaleDateString();
+  switch (type) {
+    case FIELD_TYPES.DATE:
+      const date = new Date(content);
+      return date.toLocaleDateString();
+    case FIELD_TYPES.FILE:
+      const fileName = content.substring(
+        content.lastIndexOf('/') + 1,
+        content.length
+      );
+      return fileName;
+    default:
+      return content;
   }
-
-  return content;
 };
 
 export default function FieldView({ label, content, type = FIELD_TYPES.TEXT }) {
   return (
     <div className={classes.container}>
       <label>{label}</label>
-      <p>{getFormattedContent(content, type)}</p>
+      <p className={type === FIELD_TYPES.FILE ? classes['file-container'] : ''}>
+        {getFormattedContent(content, type)}
+      </p>
     </div>
   );
 }
