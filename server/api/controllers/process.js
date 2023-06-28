b0a7d67084db37011a55958e64f031cd00d7eecc
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-28 16:49:38 +0100

    add route for creating a new process

--- server/api/controllers/process.js
+++ server/api/controllers/process.js
@@ -1,3 +1,45 @@
 const mongoose = require('mongoose');
 const Process = require('../models/process');
 const User = require('../models/user');
+
+const assignProcessToUsersInTree = async (requestTree, processId) => {
+  const users = requestTree.map(
+    (user) => new mongoose.Types.ObjectId(user.userId),
+  );
+
+  users.forEach(async (userId) => {
+    await User.findByIdAndUpdate(userId, {
+      $push: { processes: processId },
+    }).populate('processes');
+  });
+};
+
+const createProcess = async (requestData) => {
+  const { name, description, fieldSet, requestTree } = requestData;
+
+  const process = new Process({
+    _id: new mongoose.Types.ObjectId(),
+    name: name,
+    description: description,
+    fieldSet: fieldSet,
+    requestTree: requestTree,
+  });
+
+  await process.save();
+  return process._id;
+};
+
+exports.add = async (req, res) => {
+  try {
+    const processId = await createProcess(req.body);
+    await assignProcessToUsersInTree(req.body.requestTree, processId);
+
+    res.status(200).json({
+      message: 'Process created successfully!',
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
