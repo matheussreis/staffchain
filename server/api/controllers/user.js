cfcd85ed1516d82ea84109cac342c0ae52555951
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-28 16:31:01 +0100

    create routes for user login and signup

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,2 +1,126 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = require('../models/user');
+
+const findUserByEmail = async (email) => {
+  return User.find({ email: email }).exec();
+};
+
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+const createUser = async (userData) => {
+  const {
+    firstName,
+    lastName,
+    birthdate,
+    email,
+    password,
+    type,
+    role,
+    department,
+  } = userData;
+
+  const hashedPassword = await hashPassword(password);
+
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    firstName: firstName,
+    lastName: lastName,
+    birthdate: new Date(birthdate),
+    email: email,
+    password: hashedPassword,
+    department: department,
+    role: role,
+  });
+
+  user.type ??= type;
+  await user.save();
+};
+
+const getSignedToken = (userId, userEmail) => {
+  const { JWT_SECRET, JWT_TOKEN_DURATION } = process.env;
+
+  const token = jwt.sign(
+    {
+      email: userEmail,
+      userId: userId,
+    },
+    JWT_SECRET,
+    { expiresIn: `${JWT_TOKEN_DURATION || 10}h` },
+  );
+
+  return token;
+};
+
+exports.signup = async (req, res) => {
+  try {
+    const users = await findUserByEmail(req.body.email);
+
+    if (users.length > 0) {
+      res.status(409).json({
+        message: 'Account already exists.',
+      });
+
+      return;
+    }
+
+    await createUser({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      birthdate: req.body.birthdate,
+      email: req.body.email,
+      password: req.body.password,
+      department: req.body.department,
+      role: req.body.role,
+      type: req.body.type,
+    });
+
+    res.status(201).json({
+      mesage: 'User created successfully!',
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const users = await findUserByEmail(req.body.email);
+
+    if (users.length < 1) {
+      res.status(401).json({
+        message: 'Please, ensure your credentials are correct.',
+      });
+
+      return;
+    }
+
+    const passwordDidMatch = await bcrypt.compare(
+      req.body.password,
+      users[0].password,
+    );
+
+    if (passwordDidMatch) {
+      const token = getSignedToken(users[0]._id, users[0].email);
+      res.status(200).json({
+        mesage: 'Auth successful',
+        token: token,
+      });
+
+      return;
+    }
+
+    res.status(401).json({
+      message: 'Please, ensure your credentials are correct.',
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user');
+const checkAuth = require('../middlewares/check-auth');
+
+router.post('/signup', checkAuth, controller.signup);
+
+router.post('/login', controller.login);
 
 module.exports = router;
