32ac6604737336da2a8d81a2897c531a46f4c71b
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-29 23:32:02 +0100

    add admin-only items to the navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { getAuthToken } from '../../../utils/auth-utils';
 import { action as logoutAction } from '../../../pages/Logout';
+import { CurrentUserContext } from '../../../store/current-user-context';
 
 import classes from './Navbar.module.css';
 
 export default function Navbar() {
   const token = getAuthToken();
+  const { user } = useContext(CurrentUserContext);
 
   return (
     <header>
@@ -14,6 +17,19 @@ export default function Navbar() {
           {process.env.REACT_APP_NAME}
         </Link>
         <ul className={classes.list}>
+          {user.isAdmin && (
+            <>
+              <li>
+                <NavLink to="/user">Users</NavLink>
+              </li>
+              <li>
+                <NavLink to="/process">Processes</NavLink>
+              </li>
+              <li>
+                <NavLink to="/request">Requests</NavLink>
+              </li>
+            </>
+          )}
           {token && (
             <>
               <li>
