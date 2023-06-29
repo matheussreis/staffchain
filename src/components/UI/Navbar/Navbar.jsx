77fe99a2567eca6947cf3ac50a2e49232c788a88
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-29 15:13:38 +0100

    add profile and logout items to the navbar

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,13 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { getAuthToken } from '../../../utils/auth-utils';
+import { action as logoutAction } from '../../../pages/Logout';
+
 import classes from './Navbar.module.css';
 
 export default function Navbar() {
+  const token = getAuthToken();
+
   return (
     <header>
       <nav className={classes.nav}>
         <Link className={classes['site-name']} to="/">
           {process.env.REACT_APP_NAME}
         </Link>
+        <ul className={classes.list}>
+          {token && (
+            <>
+              <li>
+                <NavLink to="/me">Profile</NavLink>
+              </li>
+              <li>
+                <NavLink to="/" onClick={() => logoutAction()}>
+                  Logout
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
       </nav>
     </header>
   );
