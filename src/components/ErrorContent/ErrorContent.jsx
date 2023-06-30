6076eb1c5716813ba3707404f14bcd62b3aff956
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-06-30 14:23:18 +0100

    add redirectTo prop to the ErrorContent component

--- src/components/ErrorContent/ErrorContent.jsx
+++ src/components/ErrorContent/ErrorContent.jsx
@@ -4,14 +4,14 @@ import { useNavigate } from 'react-router-dom';
 
 import classes from './ErrorContent.module.css';
 
-const ErrorContent = ({ title, message }) => {
+const ErrorContent = ({ title, message, redirectTo = '/' }) => {
   const navigate = useNavigate();
 
   return (
     <div className={classes.container}>
       {title && <h1 className={classes.title}>{title}</h1>}
       {message && <p className={classes.message}>{message}</p>}
-      <Button onClick={() => navigate('/')}>Return</Button>
+      <Button onClick={() => navigate(redirectTo)}>Return</Button>
     </div>
   );
 };
