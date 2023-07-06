99cd7ba856cf96e52a4417492ee4bc9de57f5d08
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-06 19:39:53 +0100

    add route to get all requests and route to get a request by its id

--- server/api/controllers/request.js
+++ server/api/controllers/request.js
@@ -2,6 +2,35 @@ const mongoose = require('mongoose');
 const Request = require('../models/request');
 const { getRequestTreeByProcessId } = require('./process');
 
+const POPULATE_OPTIONS = [
+  {
+    path: 'starter',
+    select: {
+      firstName: 1,
+      lastName: 1,
+      department: 1,
+      role: 1,
+    },
+  },
+  {
+    path: 'reviewer',
+    select: {
+      firstName: 1,
+      lastName: 1,
+      department: 1,
+      role: 1,
+    },
+  },
+  {
+    path: 'process',
+    select: {
+      name: 1,
+      description: 1,
+      fieldSet: 1,
+    },
+  },
+];
+
 const createRequest = async (requestData) => {
   const { processId, starterId, fieldSet } = requestData;
 
@@ -16,7 +45,7 @@ const createRequest = async (requestData) => {
 
   const userHasNoBoss = starterNode.reportsTo === '';
   const reviewer = userHasNoBoss ? starterId : starterNode.reportsTo;
-  const status = userHasNoBoss ? 'approved' : 'to-do';
+  const status = userHasNoBoss ? 'approved' : 'in-progress';
 
   const request = new Request({
     _id: new mongoose.Types.ObjectId(),
@@ -30,6 +59,51 @@ const createRequest = async (requestData) => {
   await request.save();
 };
 
+const findAllRequests = async () => {
+  return Request.find({})
+    .populate(POPULATE_OPTIONS)
+    .sort({ dateModified: -1 })
+    .exec();
+};
+
+const findRequestById = async (id) => {
+  return Request.findById(id).populate(POPULATE_OPTIONS).exec();
+};
+
+const getRequestResponse = (request) => {
+  return {
+    id: request.id,
+    name: request.process.name,
+    description: request.process.description,
+    status: request.status,
+    comments: request.comments.map((comment) => ({
+      authorId: comment.authorId,
+      comment: comment.comment,
+    })),
+    fields: request.process.fieldSet.map((field) => ({
+      id: field._id,
+      label: field.label,
+      required: field.required,
+      type: field.type,
+      value:
+        request.fields.find((f) => f.fieldSetId.equals(field._id))
+          ?.value || '',
+    })),
+    reviewer: {
+      id: request.reviewer._id,
+      name: `${request.reviewer.firstName} ${request.reviewer.lastName}`,
+      department: request.reviewer.department,
+      role: request.reviewer.role,
+    },
+    starter: {
+      id: request.starter._id,
+      name: `${request.starter.firstName} ${request.starter.lastName}`,
+      department: request.starter.department,
+      role: request.starter.role,
+    },
+  };
+};
+
 exports.add = async (req, res) => {
   try {
     await createRequest(req.body);
@@ -37,6 +111,36 @@ exports.add = async (req, res) => {
     res.status(200).json({
       message: 'Request created successfully!',
     });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.getAll = async (req, res) => {
+  try {
+    const requests = await findAllRequests();
+
+    res.status(200).json({
+      count: requests.length,
+      requests: requests.map((request) =>
+        getRequestResponse(request),
+      ),
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+exports.get = async (req, res) => {
+  try {
+    const requestId = new mongoose.Types.ObjectId(req.params.id);
+    const request = await findRequestById(requestId);
+    const requestResponse = getRequestResponse(request);
+    res.status(200).json(requestResponse);
   } catch (error) {
     res.status(500).json({
       error: error.message,
--- server/api/routes/requests.js
+++ server/api/routes/requests.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const controller = require('../controllers/request');
 const checkAuth = require('../middlewares/check-auth');
 
+router.get('/', checkAuth, controller.getAll);
+
 router.post('/', checkAuth, controller.add);
+
+router.get('/:id', checkAuth, controller.get);
 
 module.exports = router;
