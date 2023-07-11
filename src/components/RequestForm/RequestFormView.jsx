33b0801577dc8ceff10b6d4c727b6d8a0199e4c4
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-11 21:11:10 +0100

    add logic to add comments and fix bugs

--- src/components/RequestForm/RequestFormView.jsx
+++ src/components/RequestForm/RequestFormView.jsx
@@ -1,14 +1,18 @@
-import { useContext } from 'react';
+import axios from 'axios';
 import Card from '../UI/Card/Card';
 import { Tag } from 'primereact/tag';
 import Button from '../UI/Button/Button';
 import FormRow from '../UI/Form/FormRow';
+import { Toast } from 'primereact/toast';
+import { useContext, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FieldView from '../UI/FieldView/FieldView';
 import BUTTON_SIZES from '../../enums/button-sizes';
+import { translateEnum } from '../../enums/request-status';
 import ActionButton from '../UI/ActionButton/ActionButton';
 import CommentSection from '../UI/CommentSection/CommentSection';
 import { RequestFormContext } from '../../store/request-form-context';
+import { getTagSeverityByRequestStatus } from '../../utils/enum-utils';
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 
 import classes from './RequestFormView.module.css';
@@ -70,17 +74,22 @@ const items = [
   },
 ];
 
-function RequestFormHeader({ requestName, requestStarter }) {
+function RequestFormHeader() {
   const navigate = useNavigate();
+  const { request } = useContext(RequestFormContext);
 
   return (
     <>
       <div className={classes['title-container']}>
-        <Tag value="In Progess" severity="info" className={classes.tag} />
-        <h1 className={classes.title}>{requestName}</h1>
+        <Tag
+          value={translateEnum(request.status)}
+          severity={getTagSeverityByRequestStatus(request.status)}
+          className={classes.tag}
+        />
+        <h1 className={classes.title}>{request.name}</h1>
         <div className={classes['request-starter']}>
           <label>Created By:</label>
-          <span>{requestStarter}</span>
+          <span>{request.starter.name}</span>
         </div>
       </div>
       <div className={classes['button-container']}>
@@ -90,7 +99,20 @@ function RequestFormHeader({ requestName, requestStarter }) {
           items={items}
         />
         <Button
-          onClick={() => navigate('edit')}
+          onClick={() =>
+            navigate('edit', {
+              state: {
+                id: request.id,
+                name: request.name,
+                description: request.description,
+                fields: request.fields,
+                status: request.status,
+                comments: request.comments,
+                starter: request.starter,
+                reviewer: request.reviewer,
+              },
+            })
+          }
           className={classes.button}
           size={BUTTON_SIZES.SMALL}
         >
@@ -103,29 +125,51 @@ function RequestFormHeader({ requestName, requestStarter }) {
 
 export default function RequestFormView() {
   const { request } = useContext(RequestFormContext);
+  const toastRef = useRef(null);
+
+  const addComment = async (comment) => {
+    const { REACT_APP_SERVER_API_URL: API_URL } = process.env;
+    const { id } = request;
+
+    try {
+      await axios.post(`${API_URL}/request/${id}/comment`, {
+        authorId: comment.authorId,
+        comment: comment.comment,
+        publishDate: comment.publishDate,
+      });
+    } catch (error) {
+      toastRef.current.clear();
+      toastRef.current.show({
+        severity: 'error',
+        summary: 'Add Comment Failure',
+        detail: error.message,
+        sticky: true,
+        style: { margin: '0.5rem' },
+      });
+    }
+  };
 
   return (
     <>
+      <Toast ref={toastRef} position="top-center" />
       <ConfirmDialog />
       <Card className={classes.card}>
         <FormRow className={classes['header-row']}>
-          <RequestFormHeader
-            requestName={request.name}
-            requestStarter={request.starter.name}
-          />
+          <RequestFormHeader />
         </FormRow>
         {request.fields.map((field) => (
           <FieldView
             key={field.id}
             label={`${field.name}:`}
             content={field.value}
-            type={field.type.value}
+            type={field.type}
           />
         ))}
       </Card>
       <CommentSection
         comments={request.comments}
         updateComments={request.updateComments}
+        onAddComment={addComment}
       />
     </>
   );
