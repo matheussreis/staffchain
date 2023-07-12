3d60d3f8b61b93b0e8c29db2335c11d02dfac323
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-12 22:40:11 +0100

    rename route to ask for more information

--- server/api/routes/requests.js
+++ server/api/routes/requests.js
@@ -19,7 +19,7 @@ router.put(
 
 router.post('/:id/approve', checkAuth, controller.approve);
 
-router.post('/:id/moreinfo', checkAuth, controller.moreInfo);
+router.post('/:id/more-info', checkAuth, controller.moreInfo);
 
 router.post('/:id/close', checkAuth, controller.close);
 
