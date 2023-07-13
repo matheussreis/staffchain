a837d9ae1b732f84eb3534ca6dac8b306fdf556d
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-13 21:43:27 +0100

    add anchorDisabled prop to the ListItemHeader component

--- src/components/UI/ListItem/ListItemHeader.jsx
+++ src/components/UI/ListItem/ListItemHeader.jsx
@@ -2,10 +2,17 @@ import Anchor from '../Anchor/Anchor';
 
 import classes from './ListItemHeader.module.css';
 
-export default function ListItemHeader({ title, sendTo, data }) {
+export default function ListItemHeader({
+  title,
+  sendTo,
+  data,
+  anchorDisabled = false,
+}) {
+  const Container = anchorDisabled ? 'span' : Anchor;
+
   return (
-    <Anchor className={classes['title-link']} to={sendTo} state={data}>
+    <Container className={classes['title-link']} to={sendTo} state={data}>
       <h3 className={classes.title}>{title}</h3>
-    </Anchor>
+    </Container>
   );
 }
