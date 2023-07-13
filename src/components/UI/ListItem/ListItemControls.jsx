1820468fb231de80412f37b2f5b2283a21ddadea
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-13 21:44:13 +0100

    add buttonName prop to the ListItemControls component

--- src/components/UI/ListItem/ListItemControls.jsx
+++ src/components/UI/ListItem/ListItemControls.jsx
@@ -3,11 +3,11 @@ import Button from '../Button/Button';
 
 import classes from './ListItemControls.module.css';
 
-export default function ListItemControls({ onClick }) {
+export default function ListItemControls({ onClick, buttonName = 'Open' }) {
   return (
     <div className={classes['content-container']}>
       <Button onClick={onClick} size={BUTTON_SIZES.SMALL} isAlt={false}>
-        Open
+        {buttonName}
       </Button>
     </div>
   );
