cf73e1e8e959ae35a03e27eb448265ee2848b766
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-17 22:38:25 +0100

    add isPageContent prop to the NoListItems component

--- src/components/UI/NoListItems/NoListItems.jsx
+++ src/components/UI/NoListItems/NoListItems.jsx
@@ -1,10 +1,12 @@
 import classes from './NoListItems.module.css';
 
-export default function NoListItems({ title, message }) {
+export default function NoListItems({ title, message, isPageContent = false }) {
+  const TitleComponent = isPageContent ? 'h2' : 'h4';
+
   return (
     <div className={classes.container}>
-      <h4>{title}</h4>
-      <p>{message}</p>
+      <TitleComponent className={classes.content}>{title}</TitleComponent>
+      <p className={classes.content}>{message}</p>
     </div>
   );
 }
