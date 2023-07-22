16a4d0c8af3882c999003478af461a6968a7c241
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2023-07-22 12:52:19 +0100

    add className and children props to the FormTitle component

--- src/components/UI/Form/FormTitle.jsx
+++ src/components/UI/Form/FormTitle.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 import classes from './FormTitle.module.css';
 
-export default React.memo(function FormTitle({ text }) {
-  return <h1 className={classes.title}>{text}</h1>;
+export default React.memo(function FormTitle({ text, className, children }) {
+  const getCssClasses = (initialClass = '') => {
+    let cssClasses = classes[initialClass];
+
+    if (className) {
+      cssClasses += ` ${className}`;
+    }
+
+    return cssClasses;
+  };
+
+  return <h1 className={getCssClasses('title')}>{children ?? text}</h1>;
 });
